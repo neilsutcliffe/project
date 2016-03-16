5f4eb70937781007b422d766e9f5ebb927ac02d9
Neil Sutcliffe <neil.sutcliffe@risk-technology.com>
2016-03-16 16:49:36 +0000

    Next Video!

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,10 +1,16 @@
 var React = require('react')
 
+
+function puke (obj)
+{
+	return <pre>{JSON.stringify(obj, null, '')}</pre>
+}
+
 function ConfirmBattle (props) {
 
 	return props.isLoading === true
 		? <p>Loading</p>
-		: <p>Confirm Battle</p>
+		: <p>Confirm Battle!: {puke(props)}</p>
 }
 
 module.exports = ConfirmBattle
